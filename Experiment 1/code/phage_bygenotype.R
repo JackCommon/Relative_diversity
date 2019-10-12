5ffb095a405e33482c3371de2bfa4e31c1b0c934
Jack Common <jcommon@Jacks-MacBook-Air.local>
2019-10-12 12:45:56 +0100

    added final phage by-genotype data, updated code and associated figure, and added a new by-replicate by-genotype figure

--- Experiment 1/code/phage_bygenotype.R
+++ Experiment 1/code/phage_bygenotype.R
@@ -78,7 +78,7 @@ phage$Treatment %<>% relevel(ref="3")
 
 phage %<>% na.exclude()
 
-#### ---- Figure ---- ####
+#### ---- Figure: facetted by treatment ---- ####
 # First plot the titres of each genotype
 bygenotype_plot <- ggplot(aes(y=log10(PFU+1), x=Timepoint), data=phage)+
   
@@ -135,3 +135,147 @@ last_plot()
 
 ggsave("phage_bygenotype.png", bygenotype_plot_2, path="./Experiment 1/figs/",
        device="png", dpi=600, width=28, height=20, units=c("cm"))
+
+#### ---- Figure: look at each replicate in each treatment ---- ####
+
+# Make subset dataframes by treatment
+three <- phage %>% 
+  filter(Treatment=="3")
+six <- phage %>% 
+  filter(Treatment=="6")
+twelve <- phage %>% 
+  filter(Treatment=="12")
+twentyfour <- phage %>% 
+  filter(Treatment=="24")
+
+# Set up blank plots for each treatment
+three_bygenotype_plot <- ggplot(aes(y=log10(PFU+1), x=Timepoint), data=three)+
+  
+  #geom_point(stat='identity')+
+  geom_hline(yintercept = 1, linetype=2)+
+  facet_wrap(~Replicate, nrow=2, ncol=4)+
+  
+  labs(x="Days post-infection", y=expression(bold("Pfu ml"*{}^{-1}*"")))+
+  ggtitle("A. 3-clone x 3-phage")+
+  
+  cowplot::theme_cowplot()+
+  theme(plot.title = element_text(face="bold", hjust=0, size = 16),
+        axis.title = element_text(face="bold", size=16),
+        strip.text = element_text(face='bold', size=14, lineheight = 3))+
+  
+  coord_cartesian(ylim=c(0, 12))+
+  scale_y_continuous(breaks=c(seq(0,12,2)),
+                     labels=pfu_labels)+
+  scale_colour_discrete(name=c("Phage genotype"))+
+  
+  theme(axis.text = element_text(size=12),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=16, face="bold"))+
+  NULL
+
+six_bygenotype_plot <- ggplot(aes(y=log10(PFU+1), x=Timepoint), data=six)+
+  
+  #geom_point(stat='identity')+
+  geom_hline(yintercept = 1, linetype=2)+
+  facet_wrap(~Replicate, nrow=2, ncol=4)+
+  
+  labs(x="Days post-infection", y=expression(bold("Pfu ml"*{}^{-1}*"")))+
+  ggtitle("B. 6-clone x 6-phage")+
+  
+  cowplot::theme_cowplot()+
+  theme(plot.title = element_text(face="bold", hjust=0, size = 16),
+        axis.title = element_text(face="bold", size=16),
+        strip.text = element_text(face='bold', size=14, lineheight = 3))+
+  
+  coord_cartesian(ylim=c(0, 12))+
+  scale_y_continuous(breaks=c(seq(0,12,2)),
+                     labels=pfu_labels)+
+  scale_colour_discrete(name=c("Phage genotype"))+
+  
+  theme(axis.text = element_text(size=12),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=16, face="bold"))+
+  NULL
+
+twelve_bygenotype_plot <- ggplot(aes(y=log10(PFU+1), x=Timepoint), data=twelve)+
+  
+  #geom_point(stat='identity')+
+  geom_hline(yintercept = 1, linetype=2)+
+  facet_wrap(~Replicate, nrow=2, ncol=4)+
+  
+  labs(x="Days post-infection", y=expression(bold("Pfu ml"*{}^{-1}*"")))+
+  ggtitle("C. 12-clone x 12-phage")+
+  
+  cowplot::theme_cowplot()+
+  theme(plot.title = element_text(face="bold", hjust=0, size = 16),
+        axis.title = element_text(face="bold", size=16),
+        strip.text = element_text(face='bold', size=14, lineheight = 3))+
+  
+  coord_cartesian(ylim=c(0, 12))+
+  scale_y_continuous(breaks=c(seq(0,12,2)),
+                     labels=pfu_labels)+
+  scale_colour_discrete(name=c("Phage genotype"))+
+  
+  theme(axis.text = element_text(size=12),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=16, face="bold"))+
+  NULL
+
+twentyfour_bygenotype_plot <- ggplot(aes(y=log10(PFU+1), x=Timepoint), data=twentyfour)+
+  
+  #geom_point(stat='identity')+
+  geom_hline(yintercept = 1, linetype=2)+
+  facet_wrap(~Replicate, nrow=2, ncol=4)+
+  
+  labs(x="Days post-infection", y=expression(bold("Pfu ml"*{}^{-1}*"")))+
+  ggtitle("D. 24-clone x 24-phage")+
+  
+  cowplot::theme_cowplot()+
+  theme(plot.title = element_text(face="bold", hjust=0, size = 16),
+        axis.title = element_text(face="bold", size=16),
+        strip.text = element_text(face='bold', size=14, lineheight = 3))+
+  
+  coord_cartesian(ylim=c(0, 12))+
+  scale_y_continuous(breaks=c(seq(0,12,2)),
+                     labels=pfu_labels)+
+  scale_colour_discrete(name=c("Phage genotype"))+
+  
+  theme(axis.text = element_text(size=12),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=16, face="bold"))+
+  NULL
+
+#
+
+three_bygenotype_plot <- three_bygenotype_plot + 
+  geom_path(stat='identity', aes(colour=Genotype, group=Replicate_new))+
+  theme(legend.position = "none")+
+  NULL
+last_plot()
+
+six_bygenotype_plot <- six_bygenotype_plot + 
+  geom_path(stat='identity', aes(colour=Genotype, group=Replicate_new))+
+  theme(legend.position = "none")+
+  NULL
+last_plot()
+
+twelve_bygenotype_plot <- twelve_bygenotype_plot + 
+  geom_path(stat='identity', aes(colour=Genotype, group=Replicate_new))+
+  theme(legend.position = "none")+
+  NULL
+last_plot()
+
+twentyfour_bygenotype_plot <- twentyfour_bygenotype_plot + 
+  geom_path(stat='identity', aes(colour=Genotype, group=Replicate_new))+
+  theme(legend.position = "none")+
+  NULL
+last_plot()
+
+byreplicate_plot <- plot_grid(three_bygenotype_plot+xlab(""),
+                              six_bygenotype_plot+labs(y="", x=""),
+                              twelve_bygenotype_plot, twentyfour_bygenotype_plot+ylab(""))
+quartz()
+last_plot()
+
+ggsave("phage_bygenotype_byreplicate.png", byreplicate_plot, path="./Experiment 1/figs/",
+       device="png", dpi=600, width=50, height=30, units=c("cm"))
